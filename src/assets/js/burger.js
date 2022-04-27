1ef1109c2990d7fd23a6f52bc59c1aae07ef3991
Gardex22 <86685041+Gardex22@users.noreply.github.com>
2022-04-27 17:33:13 +0700

    1.1

--- src/assets/js/burger.js
+++ src/assets/js/burger.js
@@ -1,7 +1,9 @@
 
 let btn= document.getElementById('burger-menu');
-let contentMenu=document.querySelector('.header-content');
+let contentMenu=document.querySelector('.header__menu');
+let bg=document.querySelector(".header-bg-pink")
 btn.addEventListener('click', function(){
   btn.classList.toggle('active');
-  contentMenu.classList.toggle('header-content_active');
+  contentMenu.classList.toggle('header-menu_active');
+  bg.classList.toggle('header-bg-pink_active');
 })
--- src/assets/js/slider2.js
+++ src/assets/js/slider2.js
@@ -27,3 +27,30 @@ $(".carousel").slick({
         }
       ]
 });
+$(".service-Job__carousel").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  //autoplaySpeed: 2000,
+  centerMode: true,
+  responsive: [
+      {
+        breakpoint: 978,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '40px',
+          slidesToShow: 2
+        }
+      },
+      {
+        breakpoint: 726,
+        settings: {
+          arrows: false,
+          centerMode: true,
+          centerPadding: '0px',
+          slidesToShow: 1
+        }
+      }
+    ]
+})
