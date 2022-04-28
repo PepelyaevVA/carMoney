1dfd3e1198b3023a971f9c0f48427fee98afb201
Gardex22 <86685041+Gardex22@users.noreply.github.com>
2022-04-28 21:16:42 +0700

    2.0

--- src/assets/js/slider2.js
+++ src/assets/js/slider2.js
@@ -27,12 +27,13 @@ $(".carousel").slick({
         }
       ]
 });
-$(".service-Job__carousel").slick({
-  slidesToShow: 3,
+$(".what-loans__carousel").slick({
+  slidesToShow: 2,
   slidesToScroll: 1,
-  autoplay: true,
+  //autoplay: true,
   //autoplaySpeed: 2000,
   centerMode: true,
+  arrows: false,
   responsive: [
       {
         breakpoint: 978,
@@ -40,7 +41,7 @@ $(".service-Job__carousel").slick({
           arrows: false,
           centerMode: true,
           centerPadding: '40px',
-          slidesToShow: 2
+          slidesToShow: 1
         }
       },
       {
@@ -49,6 +50,46 @@ $(".service-Job__carousel").slick({
           arrows: false,
           centerMode: true,
           centerPadding: '0px',
+          slidesToShow: 1
+        }
+      }
+    ]
+})
+
+$(".receiving__carousel").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  variableWidth: true,
+
+  arrows: false,
+  responsive: [
+      {
+        breakpoint: 726,
+        settings: {
+          arrows: false,
+          centerPadding: '30px',
+          slidesToShow: 1
+        }
+      }
+    ]
+})
+
+$(".money-fast__carousel").slick({
+  slidesToShow: 3,
+  slidesToScroll: 1,
+  autoplay: true,
+  autoplaySpeed: 2000,
+  variableWidth: true,
+
+  arrows: false,
+  responsive: [
+      {
+        breakpoint: 726,
+        settings: {
+          arrows: false,
+          centerPadding: '30px',
           slidesToShow: 1
         }
       }
